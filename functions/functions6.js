79dc36b7856686174a8e5bd0bb8a23685bbf5c99
BIT Student <classroom@bgit.rs>
2018-05-01 16:05:50 +0200

    functions6

---
+++ functions/functions6.js
@@ -0,0 +1,66 @@
+//task 1
+// function countvowels(str) {
+//     var sum = 0;
+//     for (var i = 0; i < str.length; i++) {
+//         switch (str[i]) {
+//             case "a":
+//                 sum++;
+//                 break;
+//             case "e":
+//                 sum++;
+//                 break;
+//             case "i":
+//                 sum++;
+//                 break;
+//             case "o":
+//                 sum++;
+//                 break;
+//             case "u":
+//                 sum++;
+//                 break;
+//         }
+//     }
+//     return sum;
+// }
+// console.log(countvowels("danas je lep dan"));
+
+//task 2
+
+// function combinedArray(arr1, arr2){
+//     var newArray = [];
+//     for(var i = 0; i < arr1.length; i++) { 
+//             newArray[newArray.length] = arr1[i];
+//             newArray[newArray.length] = arr2[i];
+        
+//     }
+//     return newArray;
+// }
+// var arr1 = [1,2,3];
+// var arr2 = [4,5,6];
+
+// console.log(combinedArray(arr1, arr2));
+
+//task 3
+
+// function randomArray(arr, n){
+//     newArray = [];
+//     for(var i = n; i < arr.length; i++){
+//         newArray[newArray.length] = arr[i];
+//     }
+//     for(var i = 0; i < n; i++){
+//         newArray[newArray.length] = arr[i];
+//     }
+//     return newArray;
+// }
+// var arr = [2,3,6,8,6];
+// console.log(randomArray(arr,2));
+
+//task 4
+
+function digitsArray(num){
+
+}
+
+
+
+
