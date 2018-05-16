3bdd16a3c03b9b995e6f626712859f4e8babd2fe
BIT Student <classroom@bgit.rs>
2018-05-16 17:22:51 +0200

    Events

--- DOM/DOM-js.js
+++ DOM/DOM-js.js
@@ -11,9 +11,43 @@
 // };  
 // selectLi();
 
-function removeClass() {
-    var newItem = document.querySelector(".active");
-    newItem.className = "";
-    newItem.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild.className = "active";
+// function removeClass() {
+//     var newItem = document.querySelector(".active");
+//     newItem.className = "";
+//     newItem.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild.className = "active";
+// }
+// removeClass();
+var parent = document.querySelector(".dropdown");
+var parent2 = document.querySelector(".first")
+var list = ["mercedes", "audi", "yugo", "fica"];
+var list2 = ["BMW", "Lamborghini", "Ferrari", "Aston Martin"]
+
+function appendDropdown(arr, nodeParent) {
+    var select = document.createElement("select");
+
+    arr.forEach(function (text) {
+        var option = document.createElement("option");
+        option.textContent = text;
+        select.appendChild(option);
+    });
+
+    nodeParent.appendChild(select);
 }
-removeClass();
+
+
+appendDropdown(list, parent);
+appendDropdown(list2, parent2);
+
+
+function inputValidation() {
+    var inputs = document.querySelectorAll('form label input');
+
+    inputs.forEach(function (input) {
+        if (input.hasAttribute("required") && input.value === "") {
+            input.className = "newClass";
+        }
+        else {
+            input.className ="";
+        }
+    })
+}
---
+++ Events/script.js
@@ -0,0 +1,11 @@
+
+var toggle = setInterval(function () {
+    var body = document.querySelector("body");
+    body.classList.toggle("purple");
+}, 1000);
+
+
+function turnOff() {
+    var body = document.querySelector('body');
+
+}
